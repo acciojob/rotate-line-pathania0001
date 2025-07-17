2065cb6fbc5fdf59b3c24521f024564160f5a9e2
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-17 15:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//your JS code here. If required.
+const line = document.getElementById("line");
+let rotation = 0;
 
-const line  = document.getElementById("line");
-let rotation = 0; 
-line.style.transformOrigin ="center center";
-setInterval(()=>{
-	rotation+=2;
-	line.style.transform=`rotate(${rotation%360}deg)`;
-},20)
+setInterval(() => {
+  rotation += 2;
+  line.style.transform = `translate(-50%, -50%) rotate(${rotation % 360}deg)`;
+}, 20);
