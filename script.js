ef85226721a550e37fac9a9cc89acd18bc49301b
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-17 15:15:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+const line  = document.getElementById("line");
+let rotation = 0; 
+console.log(line)
+setInterval(()=>{
+	line.style.transformOrigin ="center";
+	rotation+=2;
+	line.style.transform=`rotate(${rotation%360}deg)`;
+},20)
