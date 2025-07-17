baf0901d9150f93aa9c8bb0acbb7e247600cee6b
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-17 15:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const line = document.getElementById("line");
 let rotation = 0;
-
+console.log(line.clientWidth);
 setInterval(() => {
   rotation += 2;
   line.style.transform = `translate(-50%, -50%) rotate(${rotation % 360}deg)`;
-}, 20);
+}, 300);
